2505b79a7cdfcadaf112c53d52d1e9d9e06050bb
Maayan Ben Boher <maayan951@gmail.com>
2022-02-21 19:10:55 +0200

    140. Adding custom validators

--- DatingApp/client/src/app/register/register.component.ts
+++ DatingApp/client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { AccountService } from './../services/account.service';
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ export class RegisterComponent implements OnInit {
   model: any ={};
   @Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter<boolean>();
+  registerForm: FormGroup;
 
   constructor(
     private accountService: AccountService,
@@ -18,22 +20,49 @@ export class RegisterComponent implements OnInit {
     ) { }
 
   ngOnInit() {
+    this.initializeForm();
   }
 
   register(){
-    this.accountService.register(this.model).subscribe(
-      (data) => {
-        console.log(data);
-        this.cancel();
-      },
-      error => {
-        this.toastr.error(error.error);
-        console.log(error);
-      }
-    )
+
+    // this.accountService.register(this.model).subscribe(
+    //   (data) => {
+    //     console.log(data);
+    //     this.cancel();
+    //   },
+    //   error => {
+    //     this.toastr.error(error.error);
+    //     console.log(error);
+    //   }
+    // )
+
+    console.log(this.registerForm.value);
+
   }
 
   cancel(){
     this.cancelRegister.emit(false);
   }
+
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl("Hello", Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')] )
+    });
+    this.registerForm.get('password')?.valueChanges.subscribe(() =>{
+      this.registerForm.get('confirmPassword')?.updateValueAndValidity();
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const controlValue = control.value;
+      const controlToMatch = (control?.parent as FormGroup)?.controls[matchTo];
+      const controlToMatchValue = controlToMatch?.value;
+      return controlValue === controlToMatchValue ? null : { isMatching: true };
+    }
+  }
+
+
 }
